a4956f26829c2de832c1ea301f8062fbc276ff06
Robert Wiesner <robert@scalable.capital>
2020-08-12 00:26:41 +0200

    chore: add missing files

---
+++ cloud-security/src/main/kotlin/com/scacap/cloudsecurity/CloudSecret.kt
@@ -0,0 +1,12 @@
+package com.scacap.cloudsecurity
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.cloud.context.config.annotation.RefreshScope
+
+@RefreshScope
+@ConfigurationProperties("vault")
+class CloudSecret {
+
+    lateinit var shared: String
+}
+
